6398c3368d8233950cd814777927eeb666694c8e
weizeng <466730846@qq.com>
2018-03-12 12:02:48 +0800

    调试interface‘

---
+++ interface.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type MyFloat float64
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64  {
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func (f MyFloat) Abs() float64  {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3,4}
+
+	a = f
+	a = &v
+
+	//a = v
+
+	fmt.Println(a.Abs())
+
+
+}
+
+
+
